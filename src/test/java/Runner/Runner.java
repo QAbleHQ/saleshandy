230cd068c9a3ec1982e3d2e8d25074a3d5ccf0c8
Viral Patel <viral@nevvon.com>
2022-06-27 17:55:22 +0530

    demo all the api

--- src/test/java/Runner/Runner.java
+++ src/test/java/Runner/Runner.java
@@ -4,7 +4,6 @@ import io.unity.framework.runner.TestRunner;
 
 public class Runner {
 
-
     public static void main(String[] args) {
         TestRunner run = new TestRunner();
         run.start(args);
--- src/test/java/api/test/SignUpOtpTest.java
+++ src/test/java/api/test/SignUpOtpTest.java
@@ -1,80 +1,54 @@
 package api.test;
 
+import api.requests.Sessions.GmailSession;
+import io.unity.framework.init.Session;
 import io.unity.framework.init.base;
 import io.unity.performaction.autoapi.RequestBuilder;
 import io.unity.performaction.autoapi.ResponseValidator;
 import org.json.simple.JSONObject;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SignUpOtpTest extends base {
 
-    public void gmaillogin()
-    {
-        driver.switchTo().newWindow(WindowType.TAB);
-        driver.navigate().to("https://gmail.com");
-    }
-
-    public void click_on_gmail_subject_text(){
-        List<WebElement> gmail_subject_text1 = driver.findElements(By.xpath("//span[contains(.,'Saleshandy')]"));
-        for(WebElement emailsub1 : gmail_subject_text1){
-            if(emailsub1.getText().equals("Saleshandy") == true){
-                System.out.println(emailsub1);
-                wait.wait_for_second(5);
-                emailsub1.click();
-                wait.wait_for_second(5);
-                break;
-            }
-        }
-    }
-
-    public String get_otp() {
-        wait.wait_for_second(5);
-        //verify.element_text_is_equal_to("otp_link", link_text);
-        //Pattern pattern = Pattern.compile("numFound=\"([0-9]+)\"");
-        //Matcher matcher = pattern.matcher("");
-
-        //if (matcher.find()) {
-        //System.out.println(matcher.group(1));
-
-        //}
-        String otp_string = driver.findElement(By.xpath("//div[@class= \"a3s aiL \"]//div")).getText();
-        String final_otp = otp_string.replaceAll("[^0-9]","");
-        System.out.println(final_otp);
-        wait.wait_for_second(5);
-        ArrayList<String> newTab = new ArrayList<String>(driver.getWindowHandles());
-        String oldTab = driver.getWindowHandle();
-        driver.switchTo().window(newTab.get(0));
-        wait.wait_for_second(5);
-        WebElement enter_otp = driver.findElement(By.xpath("//input[@placeholder = \"Enter OTP sent to your email\"]"));
-        enter_otp.sendKeys(final_otp);
-        wait.wait_for_second(5);
-        return final_otp;
-    }
-
-
-
     @Test
     public void signup_with_valid_otp() {
 
         RequestBuilder builder = new RequestBuilder();
-        JSONObject response = builder.performRequest("verifysignup/signup_with_valid_otp");
-        response.put("code",final_otp);
+        JSONObject response = builder.performRequest("signup/signup_with_valid_credentials");
 
+        GmailSession session = new GmailSession();
+
+        String final_otp = session.generate_otp();
+
+        response.put("code", final_otp);
 
 
         ResponseValidator signup = new ResponseValidator(response);
         signup.statusCodeShouldBe(201);
 
-    }
+        Map header = new HashMap();
+        header.put("Content-Type","application/json");
 
+        Map body = new HashMap();
+        body.put("email","qabletest+1@gmail.com");
+        body.put("code",final_otp);
+
+
+        JSONObject otpResponse =    builder.performRequestWithHeaderAndBody("verifysignup/signup_with_valid_otp",header,body);
+
+        System.out.println(otpResponse);
+    }
 
 
 }
